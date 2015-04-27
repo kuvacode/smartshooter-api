a80aea58ab557c83da81a7a13cfccbafec70a002
Francis Hart <francis@hartcw.com>
2015-04-27 22:52:04 +0300

    Add util script for manipulating shutter button state

---
+++ utils/smartshooter-shutterbutton.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2015, Kuvacode Oy. All rights reserved.
+#
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import sys
+import json
+import argparse
+import zmq
+
+def send_shutterbutton(socket, state):
+    req = {}
+    req["msg_type"] = "Request"
+    req["msg_id"] = "SetShutterButtonMsg"
+    req["msg_ref_num"] = 0
+    req["CameraSelection"] = "All"
+    req["CameraShutterButton"] = state
+    socket.send_string(json.dumps(req))
+    rep = socket.recv()
+    str_msg = rep.decode("utf-8")
+    json_msg = json.loads(str_msg)
+    return json_msg["Result"]
+
+def main():
+    parser = argparse.ArgumentParser("smartshooter-trigger.py [options]")
+    parser.add_argument("-s", "--state",
+                        required=True,
+                        choices=["off", "half", "full"],
+                        help="specify the desired button state")
+    parser.add_argument("-r", "--reqrep",
+                        default="tcp://127.0.0.1:54544",
+                        metavar="ENDPOINT",
+                        help="specify ZMQ address of Smart Shooter request/reply server")
+    args = parser.parse_args()
+
+    context = zmq.Context()
+
+    req_socket = context.socket(zmq.REQ)
+    req_socket.connect(args.reqrep)
+
+    if not send_shutterbutton(req_socket, args.state):
+        print("Failed to send shutter button message", file=sys.stderr)
+        exit(1)
+
+if __name__ == "__main__":
+    main()
